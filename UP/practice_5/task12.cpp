916bb26d638a1367f39941f59d045a2ca7a048ff
Yasen Alexiev <yassen4o222@abv.bg>
2017-11-13 23:03:34 +0200

    Fix task12 from practice 5

--- UP/practice_5/task12.cpp
+++ UP/practice_5/task12.cpp
@@ -5,15 +5,11 @@ using namespace std;
 int main() {
 	unsigned int number;
 	cin >> number;
-	int i = 2;
-	while (number >= 0) {
-		if (number%i == 0) {
-			do {
-				cout << i << " ";
-				number /= i;
-			} while (number%i == 0);
+	for (int i = 2; i <= number; i++) {	
+		 while (number%i == 0) {
+			cout << i << " ";
+			number /= i;
 		}
-		++i;
 	}
 
 	return 0;
